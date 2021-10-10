0293e1f24b7f8ebf61a19cbd6f7c46e32e3462a5
Adrian <sradri2000@gmail.com>
2021-10-10 22:16:57 +0200

    Añadiendo todo el proyecto actual

---
+++ HandPose/pose.py
@@ -0,0 +1,90 @@
+import mediapipe as mp
+import cv2
+
+mp_pose = mp.solutions.pose # Es el conjunto de las herramientas de mediapipe.
+
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+
+
+with mp_pose.Pose(
+    static_image_mode = False, # False por defecto.
+    model_complexity = 1, # 1 por defecto.
+    smooth_landmarks = True, # True por defecto.
+    min_detection_confidence = 0.5, # 0.5 por defecto.
+    min_tracking_confidence = 0.5 # 0.5 por defecto.
+    ) as pose:
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+			# If loading a video, use 'break' instead of 'continue'.
+            continue
+
+		# Flip the image horizontally for a later selfie-view display, and convert the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        height, width, _ = image.shape
+		# To improve performance, optionally mark the image as not writeable to pass by reference.
+        image.flags.writeable = False
+
+        # Obtain the results of the detection.
+        results = pose.process(image)
+
+		# Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.pose_landmarks:
+            # LEFT:
+            x1 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x * width)
+            y1 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y * height)
+            z1 = float(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].z)
+
+            x2 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].x * width)
+            y2 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y * height)
+            x3 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x * width)
+            y3 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y * height)
+
+            cv2.line(image, (x1,y1), (x2,y2), (0,255,0), 3)
+            cv2.line(image, (x2,y2), (x3,y3), (0,255,0), 3)
+            cv2.circle(image, (x1,y1), 6, (0,0,255), -1)
+            cv2.circle(image, (x2,y2), 6, (0,0,255), -1)
+            cv2.circle(image, (x3,y3), 6, (0,0,255), -1)
+
+
+            # RIGHT:
+            x1 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * width)
+            y1 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y * height)
+            x2 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].x * width)
+            y2 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y * height)
+            x3 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x * width)
+            y3 = int(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y * height)
+
+            cv2.line(image, (x1,y1), (x2,y2), (255,0,0), 3)
+            cv2.line(image, (x2,y2), (x3,y3), (255,0,0), 3)
+            cv2.circle(image, (x1,y1), 6, (0,0,255), -1)
+            cv2.circle(image, (x2,y2), 6, (0,0,255), -1)
+            cv2.circle(image, (x3,y3), 6, (0,0,255), -1)
+
+            print(z1)
+
+        cv2.imshow("imagen", image)
+        if cv2.waitKey(5) & 0xFF == 27: # If pulse ESC.
+            break
+
+
+cap.release()
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Proyecto-TFG
-Pos mi tfg tts
+Este es mi primera prueba tanto con Git como en Github.
 
-Hola mundo
+El objetivo es aprender a utilizar un gestor de versiones y además un repositorio online para usarlo como copia de seguridad de mi proyecto para el TFG de ingeniería ROBÓTICA en la UA.
+
+El objetivo del TFG es desarrollar una interfad de control hombre máquina de bajo coste para controlar brazos robot mediante visión por computador.
+
+## De momento los objetivos son:
+
+- Seguir testeando con MediaPipe.
+- Ver si utilizar la Z que proporciona es factible para la profundidad.
+- Probar a obtener las coordenaadas articulares y pasárselo a una simulación de un brazo robot.
+- Desarrollar una simulación mediante PeterCorke en python3.
+- Probar a hacer una simulación más detallada en Swift.
